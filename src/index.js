2541c0db8aafa6709033333100ac0ef57fbd3ab9
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-08-24 09:32:26 +0300

    Раскомментировал часть с итератором. Надеюсь, все верно.

--- src/index.js
+++ src/index.js
@@ -3,31 +3,31 @@ import './js/app';
 import { fellowship, pack, aragorn, gandalf, legolas } from './js/app'
 
 
-let setIter = pack.values()
-setIter.next()
-setIter.next()
+// let setIter = pack.values()
+// setIter.next()
+// setIter.next()
 
 
-// const iterator = {
-//   gen(n = pack.size) {
-//     let i = 1;
-//     return {
-//       next() {
-//       if (i<=n) {
-//         return { value: i++, done: false }
-//       } else {
-//         return { value: undefined, done: true }
+const iterator = {
+  gen(n = pack.size) {
+    let i = 1;
+    return {
+      next() {
+      if (i<=n) {
+        return { value: i++, done: false }
+      } else {
+        return { value: undefined, done: true }
         
 
-//       }
-//     }
-//   }
-//   }
-// }
+      }
+    }
+  }
+  }
+}
 
-// let a = iterator.gen()
-// a.next()
-// a.next()
-// a.next()
-// a.next()
+let a = iterator.gen()
+a.next()
+a.next()
+a.next()
+a.next()
 
